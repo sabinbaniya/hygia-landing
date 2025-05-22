60a1a242ddcbe5c7529ad7abf92fb0b7b694787e
sabinbaniya <dummymail@tutanota.de>
2025-05-22 15:22:51 +0545

    feat: initial hero section landing page

---
+++ src/lib/fonts/body/index.ts
@@ -0,0 +1,6 @@
+import { Inter } from "next/font/google";
+
+export const BodyFont = Inter({
+  variable: "--font-body",
+  subsets: ["latin"],
+});
---
+++ src/lib/fonts/primary/index.ts
@@ -0,0 +1,96 @@
+import LocalFont from "next/font/local";
+
+export const PrimaryFont = LocalFont({
+  src: [
+    {
+      path: "./MonaSansCondensed-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./MonaSansCondensed-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "./MonaSansCondensed-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./MonaSansCondensed-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./MonaSansCondensed-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./MonaSansCondensed-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "./MonaSansCondensed-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./MonaSansCondensed-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./MonaSansCondensed-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./MonaSansCondensed-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./MonaSansCondensed-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./MonaSansCondensed-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./MonaSansCondensed-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./MonaSansCondensed-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    // {
+    //   path: "./MonaSansCondensed-Thin.ttf",
+    //   weight: "200",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "./MonaSansCondensed-ThinItalic.ttf",
+    //   weight: "200",
+    //   style: "italic",
+    // },
+    // {
+    //   path: "./MonaSansCondensed-UltraLight.ttf",
+    //   weight: "100",
+    //   style: "normal",
+    // },
+    // {
+    //   path: "./MonaSansCondensed-UltraLightItalic.ttf",
+    //   weight: "100",
+    //   style: "italic",
+    // },
+  ],
+});
