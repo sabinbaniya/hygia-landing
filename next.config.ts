5ab3217b4ba19def88b19dc3029911ec64de8cc8
biratdevpoudel <be2019se669@gces.edu.np>
2025-06-29 21:58:33 +0545

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_PRODUCTION_URL: "https://www.hygialens.com",
+    NEXT_PUBLIC_BACKEND_PRODUCTION_URL: "https://api.hygialens.com/",
+  },
 };
 
 export default nextConfig;
