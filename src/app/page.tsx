207c6c96b452c78ac54a8749f58b96226a3f6cae
biratdevpoudel <be2019se669@gces.edu.np>
2025-06-30 22:14:09 +0545

    feat: add new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import CoreValue from "@/components/sections/core-value";
 import FaqSection from "@/components/sections/faq";
 import HeroSection from "@/components/sections/hero";
+import { NewsletterSubscribe } from "@/components/sections/newsletter-subscribe";
 import StatsSection from "@/components/sections/stats";
 import { Fragment } from "react";
 
@@ -8,7 +12,9 @@ export default function Home() {
     <Fragment>
       <HeroSection />
       <StatsSection />
+      <CoreValue />
       <FaqSection />
+      <NewsletterSubscribe />
     </Fragment>
   );
 }
