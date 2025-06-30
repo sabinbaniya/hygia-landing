c1da1d8b662b30d02372237f91354e2544001dd9
biratdevpoudel <be2019se669@gces.edu.np>
2025-06-30 21:38:39 +0545

    chore: create seperate env file

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NEXT_PUBLIC_PRODUCTION_URL: "https://www.hygialens.com",
-    NEXT_PUBLIC_BACKEND_PRODUCTION_URL: "https://api.hygialens.com/",
-  },
+  env: {},
 };
 
 export default nextConfig;
