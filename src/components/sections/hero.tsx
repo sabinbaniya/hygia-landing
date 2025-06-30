256c7584adba6fa833a58fce77eeddc262a35b12
biratdevpoudel <be2019se669@gces.edu.np>
2025-06-30 22:15:17 +0545

    feat: update hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { GetItOnAppleStore } from "../svg/get-it-on-apple-store";
 import { GetItOnGooglePlay } from "../svg/get-it-on-google-play";
@@ -5,19 +7,30 @@ import { PrimaryFont } from "@/lib/fonts/primary";
 import { WordRotate } from "../magicui/word-rotate";
 import { Layout } from "../layout";
 import { HeroCards } from "../common/hero-cards";
+import { useAuth } from "@/context/AuthContext";
 
 const HeroSection = () => {
+  const { user } = useAuth();
+  console.log(user);
   return (
     <div className="w-full h-screen sticky bg-gradient-to-br from-[#3ec76e] via-[#7ddc65] to-[#3ec76e]">
       <Layout>
         <div className="flex flex-row items-end justify-end gap-6">
           <div className="flex flex-col items-start w-2/3">
             <div className="bg-white p-2 rounded-br-xl rounded-tr-xl rounded-bl-xl">
-              <h1
-                className={`${PrimaryFont.className} font-bold py-2 text-5xl lg:text-7xl text-[#1E5233]`}
-              >
-                Hygia Lens
-              </h1>
+              {user?.role !== "authenticated" ? (
+                <h1
+                  className={`${PrimaryFont.className} font-bold py-2 text-xl lg:text-3xl text-[#1E5233]`}
+                >
+                  Welcome 👋,
+                </h1>
+              ) : (
+                <h1
+                  className={`${PrimaryFont.className} font-bold py-2 text-xl lg:text-3xl text-[#1E5233]`}
+                >
+                  Welcome 👋, {user?.email!.split("@")[0]}
+                </h1>
+              )}
             </div>
             <h1
               className={`${PrimaryFont.className} font-bold py-2 text-5xl lg:text-7xl bg-gradient-to-br from-black to-black/70 text-transparent bg-clip-text`}
