6859adf9aceda7a6450b7519af284fa3ee2799f6
biratdevpoudel <be2019se669@gces.edu.np>
2025-07-04 14:37:22 +0545

    chore(config): add pwa config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/app",
+  sw: "service-worker.js",
+  extendDefaultRuntimeCaching: true,
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  env: {},
-};
-
-export default nextConfig;
+// Your Next config is automatically typed!
+module.exports = withPWA({
+  // Your Next.js config
+});
